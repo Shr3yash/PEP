66d63682a65099ea33d29c4b37ef7ef2ddaa4a03
SHBHATKA-54FZSV\Shreyash <shreyash.bhatkar@oracle.com>
2024-09-14 00:31:45 +0530

    Inclusion of QuarterData Graph.

--- PEP_Web_Application/src/js/viewModels/dashboard.js
+++ PEP_Web_Application/src/js/viewModels/dashboard.js
@@ -1,27 +1,48 @@
-define(['../accUtils', 'knockout'], function(accUtils, ko) {
-  function DashboardViewModel() {
-      // Observable to control button's disabled state
-      this.disableControls = ko.observable(false);
+define([
+  "require",
+  "exports",
+  "knockout",
+  "ojs/ojbootstrap",
+  "ojs/ojarraydataprovider",
+  "ojs/ojknockout",
+  "ojs/ojchart",
+  "text!../data/quarterDataDemo.json",  // Updated path to JSON file
+  // "jet-composites/demo-chart-orientation-control/loader"
+], function (require, exports, ko, ojbootstrap_1, ArrayDataProvider, quarterData) {
+  "use strict";
+  
+  class DashboardViewModel {
+      constructor() {
+          // Existing ViewModel properties and methods
+          this.disableControls = ko.observable(false);
 
-      // ViewModel lifecycle methods
-      this.connected = () => {
-          accUtils.announce('Dashboard page loaded.', 'assertive');
-          document.title = "Dashboard";
-      };
+          // Chart-related properties
+          this.orientationValue = ko.observable('vertical');
+          // this.dataProvider = new ArrayDataProvider(JSON.parse(quarterData), {
+          //     keyAttributes: 'id'
+          // });
+      }
 
-      this.disconnected = () => {
+      connected() {
+          ojbootstrap_1.whenDocumentReady().then(() => {
+              ko.applyBindings(this, document.getElementById('chart-container'));
+              accUtils.announce('Dashboard page loaded.', 'assertive');
+              document.title = "Dashboard";
+          });
+      }
+
+      disconnected() {
           // Implement if needed
-      };
+      }
 
-      this.transitionCompleted = () => {
+      transitionCompleted() {
           // Implement if needed
-      };
+      }
 
-      // Example: toggle the button state
-      this.toggleButton = () => {
-          this.disableControls(!this.disableControls()); // Toggles between true and false
-      };
+      toggleButton() {
+          this.disableControls(!this.disableControls());
+      }
   }
-
+  
   return DashboardViewModel;
 });
